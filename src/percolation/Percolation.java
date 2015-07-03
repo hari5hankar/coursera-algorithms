b0ca71584c7a478dc8d8d9bdc3c6e91419bc89d5
harishankar-vishwanathan <harishankar.vishwanathan@gmail.com>
2015-07-02 21:41:08 -0400

    backwash

--- src/percolation/Percolation.java
+++ src/percolation/Percolation.java
@@ -2,9 +2,9 @@ package percolation;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-
 public class Percolation {
-    private WeightedQuickUnionUF wuf;
+    private WeightedQuickUnionUF wuf1;
+    private WeightedQuickUnionUF wuf2;
     private boolean[] open;
     private int N;
     
@@ -14,19 +14,20 @@ public class Percolation {
     public Percolation(int N) {
         if (N <= 0) throw new IllegalArgumentException();
         this.N = N;
-        wuf = new WeightedQuickUnionUF(N * N + 2);
+        wuf1 = new WeightedQuickUnionUF(N * N + 2);
+        wuf2 = new WeightedQuickUnionUF(N * N + 1);
         open = new boolean[N * N + 2];
     }
 
-    private boolean areIndicesValid(int i , int j) {
-        return !(i < 1 || j < 1 || i > N || j > N);
+    private boolean indicesAreInvalid(int i , int j) {
+        return (i < 1 || j < 1 || i > N || j > N);
     }
     
     /*
-     * returns index in a 1-d array addressed by 2-d indices.
+     * returns index in a 1-d array addressed by 2-d indices (i * N + j)
      */
     private int indexOf(int i, int j) {
-        if (!areIndicesValid(i, j)) throw new IndexOutOfBoundsException();
+        if (indicesAreInvalid(i, j)) throw new IndexOutOfBoundsException();
         return (i - 1) * N + (j - 1);
     }
     
@@ -34,37 +35,44 @@ public class Percolation {
      * Opens site (row i, column j) if it is not open already
      */
     public void open(int i, int j) {
+       // StdOut.println();
         
-        if (!areIndicesValid(i, j)) throw new IndexOutOfBoundsException();
+        if (indicesAreInvalid(i, j)) throw new IndexOutOfBoundsException();
         
         int p = indexOf(i, j);
+        
         open[p] = true;
         
         if (i == 1) {
-            wuf.union(indexOf(i, j), N * N);
+            wuf1.union(p, N * N);
+            wuf2.union(p, N * N);
         }
         if (i == N) {
-            wuf.union(indexOf(i, j), N * N + 1);
+            wuf1.union(p, N * N + 1);
         }
         
         if (i > 1) {
             if (isOpen(i - 1, j)) {
-                wuf.union(indexOf(i, j) , indexOf(i - 1 , j));
+                wuf1.union(p , indexOf(i - 1 , j));
+                wuf2.union(p , indexOf(i - 1 , j));
             }
         }
         if (j > 1) {
             if (isOpen(i, j - 1)) {
-                wuf.union(indexOf(i, j) , indexOf(i, j - 1));
+                wuf1.union(p , indexOf(i, j - 1));
+                wuf2.union(p , indexOf(i, j - 1));
             }
         }
         if (i < N) {
             if (isOpen(i + 1, j)) {
-                wuf.union(indexOf(i, j) , indexOf(i + 1 , j));
+                wuf1.union(p , indexOf(i + 1 , j));
+                wuf2.union(p , indexOf(i + 1 , j));
             }
         }
         if (j < N) {
             if (isOpen(i, j + 1)) {
-                wuf.union(indexOf(i, j) , indexOf(i , j + 1));
+                wuf1.union(p , indexOf(i , j + 1));
+                wuf2.union(p , indexOf(i , j + 1));
             }
         }
        
@@ -74,7 +82,7 @@ public class Percolation {
      * Is site (row i, column j) open? 
      */
     public boolean isOpen(int i, int j) {
-        if (!areIndicesValid(i, j)) throw new IndexOutOfBoundsException();
+        if (indicesAreInvalid(i, j)) throw new IndexOutOfBoundsException();
         return open[indexOf(i, j)];
     }
     
@@ -82,17 +90,21 @@ public class Percolation {
      * Is site (row i, column j) full?
      */
     public boolean isFull(int i, int j) {
-        if (!areIndicesValid(i, j)) throw new IndexOutOfBoundsException();
-        return wuf.connected(indexOf(i, j), N * N);
+        if (indicesAreInvalid(i, j)) throw new IndexOutOfBoundsException();
+        //StdOut.print(wuf1.find(indexOf(i, j)) + " ");
+        return wuf2.connected(indexOf(i, j), N * N);
     }
     
     /*
      * Does the system percolate?
      */
     public boolean percolates() {
-        return wuf.connected(N * N , N * N + 1);
+        return wuf1.connected(N * N , N * N + 1);
     }
     
+    /*
+     * optional test client
+     */
     public static void main(String[] args) {
         
     }
