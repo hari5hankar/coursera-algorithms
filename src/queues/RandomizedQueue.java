3896372b7f10c756993d09e3e347231a37a67719
harishankar-vishwanathan <harishankar.vishwanathan@gmail.com>
2015-07-09 23:56:38 -0400

    randomized queue

--- src/queues/RandomizedQueue.java
+++ src/queues/RandomizedQueue.java
@@ -1,63 +1,68 @@
 package queues;
 
+import java.util.Iterator;
+
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
     /*
      * construct an empty randomized queue
      */
-    public RandomizedQueue(){
-        
+    public RandomizedQueue() {
+
     }
-    
+
     /*
      * is the queue empty?
      */
-    public boolean isEmpty(){
-        
+    public boolean isEmpty() {
+
+        return false;
     }
-    
-    
+
     /*
      * return the number of items on the queue
      */
-    public int size(){
-        
+    public int size() {
+
+        return 0;
     }
-    
+
     /*
      * add the item
      */
     public void enqueue(Item item) {
-        
+
     }
-    
+
     /*
      * remove and return a random item
      */
     public Item dequeue() {
-        
+
+        return null;
     }
-    
-    
+
     /*
      * return (but do not remove) a random item
      */
     public Item sample() {
-        
+
+        return null;
     }
-    
+
     /*
      * return an independent iterator over items in random order
      */
     public Iterator<Item> iterator() {
-        
+
+        return null;
     }
-    
+
     /*
      * unit testing
      */
     public static void main(String[] args) {
-        
+
     }
-    
+
 }
