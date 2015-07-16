7c54c49425c2e8a10f9e2cb125b962ccf0a56f5c
harishankar-vishwanathan <harishankar.vishwanathan@gmail.com>
2015-07-16 19:36:21 -0400

    reservoir sampling to reduce size of queue

--- queues/Subset.java
+++ queues/Subset.java
@@ -1,18 +1,25 @@
-import java.util.Iterator;
-
 public class Subset {
 
     public static void main(String[] args) {
-        RandomizedQueue<String> rq = new RandomizedQueue<>();
         int k = Integer.parseInt(args[0]);
+        int count = 0;
+        RandomizedQueue<String> rq = new RandomizedQueue<String>();
         while (!StdIn.isEmpty()) {
-            rq.enqueue(StdIn.readString());
+            String input = StdIn.readString();
+            if (count >= k) {
+                int j = StdRandom.uniform(0, count + 1);
+                if (j < k) {
+                    rq.dequeue();
+                    rq.enqueue(input);
+                }
+            }else{
+                rq.enqueue(input);
+            }
+            count++;
         }
-        Iterator<String> iter = rq.iterator();
-        while (k > 0) {
-            StdOut.println(iter.next());
-            k--;
+        for(String s:rq){
+            StdOut.println(s);
         }
-
     }
+
 }
